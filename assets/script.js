bf8498f2a142eea010187dca3b5e5af12c240231
Norman Herrera <norman.herrera@ymail.com>
2022-03-17 23:20:46 -0700

    made a checklist

---
+++ assets/script.js
@@ -0,0 +1,16 @@
+// Make start function
+// 1. Start a timer
+//      a. timer should be 10 mins
+//      b. When 10 mins are over, timer clears and goes to view results page.
+// 2. Render out quiz questions 1 by 1.
+// 3. Save each right or wrong answer for each turn.
+// 4. display final result.
+// 5. add final result to a tally of wins and losses stored locally.
+
+// While the quiz is in progress
+// 1. Add event listeners to each answer choice.
+// 2. Correct answer choices will add to correct side
+// 3. Wrong answer choices will add to wrong side. 
+// 4. Event listeners should also display the next question. 
+// 5. Event listeners should also display whether the answer was right or wrong.
+
